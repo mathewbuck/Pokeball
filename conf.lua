9beb309c1245107f62a427dd3c24127565bacaf2
Loafz <mathewbuck38@gmail.com>
2021-01-03 19:00:35 -0800

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,9 @@
+
+function love.conf(t)
+    t.title = "Pokeball"                
+    t.version = "11.3"                   
+    t.console = false                     
+    t.window.width = 600                 
+    t.window.height = 600                 
+    t.window.vsync = 0
+end
---
+++ main.lua
@@ -0,0 +1,20 @@
+
+
+function love.load()
+	print("Use the right or left arrow keys to make the pokeball move")
+	pokeball = love.graphics.newImage("pokeball.png")
+	alpha = 0
+end
+
+function love.draw()
+	love.graphics.clear(0.0,0.0,.0)
+	love.graphics.draw(pokeball, 300,300, alpha, 1, 1, 200, 200)
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("left") then
+		alpha = alpha -dt
+elseif love.keyboard.isDown("right") then
+		alpha = alpha +dt
+	end
+end
